ced3a336527d7ff62037b267882520fc87c40fad
Thiago <thiago.wander22@gmail.com>
2024-07-15 23:52:37 +0200

    🚧 better error handling for homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
-
 import { useOrganizationsContext } from '../contexts/organizationContext';
 
-
 const HomePage = () => {
+    const context = useOrganizationsContext();
 
-    const { organizations, isLoading } = useOrganizationsContext();
+    // Verificar se o contexto está definido
+    if (!context) {
+        return <div>Erro: Contexto de organizações não encontrado.</div>;
+    }
+
+    const { organizations, isLoading } = context;
 
     return (
-            <div>
-                <h1>Bem-vindo à Página Inicial</h1>
-                <p>Você fez login com sucesso!</p>
-                {isLoading ? (
-                    <p> Carregando Oranizações... </p>
-                ) : (
-                    <ul>
-                        {organizations.map(org => (
-                            <li key={org.organizationId}>{org.organizationName}</li>
-                        ))}
-                    </ul>
-                )}
-            </div>
+        <div>
+            <h1>Bem-vindo à Página Inicial</h1>
+            <p>Você fez login com sucesso!</p>
+            {isLoading ? (
+                <p>Carregando Organizações...</p>
+            ) : organizations && organizations.length > 0 ? (
+                <ul>
+                    {organizations.map(org => (
+                        <li key={org.organizationId}>{org.organizationName}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Nenhuma organização encontrada.</p>
+            )}
+        </div>
     );
 };
-
 
 export default HomePage;
